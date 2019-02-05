96a2402360e8163213555128b63f6c2ced5f8c7c
Rishikesh Agrawani <hygull@Rishikeshs-MacBook-Air.local>
2019-02-05 23:10:03 +0530

    added hello world example

--- basic/helloworld.go
+++ basic/helloworld.go
@@ -1,8 +1,3 @@
-# Golang
-
-<h3 id='1'>A simple hello world program</h3>
-
-```go
 /**
 	{
 		"created": "5 Feb 2019, Tue",
@@ -22,18 +17,3 @@ func main() {
 	// Writing message to console using "Println()" function defined in "fmt" package
 	fmt.Println("Hello programmers, this is really a great chance to learn Golang")
 }
-```
-
-> **Output**
-
-```bash
-Rishikeshs-MacBook-Air:basic hygull$ pwd
-/Users/hygull/Projects/Golang/golang/basic
-Rishikeshs-MacBook-Air:basic hygull$ 
-Rishikeshs-MacBook-Air:basic hygull$ ls
-helloworld.go
-Rishikeshs-MacBook-Air:basic hygull$ go run helloworld.go 
-Hello programmers, this is really a great chance to learn Golang
-Rishikeshs-MacBook-Air:basic hygull$ 
-```
-
